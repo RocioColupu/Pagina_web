cfd76e669f14629d5ccf9fc7ead8857072057198
pierodioses <pieroaromm2004@gmail.com>
2023-08-28 00:21:01 -0500

    correccion conexion

--- conexion.php
+++ conexion.php
@@ -1,13 +1,19 @@
 <?php
-    // Excepciones
-    try {
-        $usuario="root";
-        $contraseña="";
-        // Conexion        
-        $mbd = new PDO('mysql:host=localhost;dbname=Aedima', $usuario, $contraseña);        
-        // En caso de presentar error
-    } catch (PDOException $e) {
-        print "¡Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
+$contraseña="";
+$usuario="root";
+$nombre_bd="crud";
+
+try {
+    $bd=new PDO (
+        'mysql:host=localhost;
+        AEDIMA='. $nombre_bd,
+        $usuario,
+        $contraseña,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+
+
+    );
+} catch (Exception $e) {
+    echo "Problema con la conexion". $e ->getMessage();
+}
 ?>
