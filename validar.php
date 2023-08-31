5d601093b675773ec90582d73cae78889f8c8b92
Roci <pprado292@gmail.com>
2023-08-30 23:49:31 -0500

    Sesiones

--- validar.php
+++ validar.php
@@ -1,1 +1,25 @@
+<?php
+session_start();
 
+include ('conexion.php');
+
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$validar_user = mysqli_query($conexion, "SELECT * FROM usuario WHERE User='$usuario' and Clave='$password'");
+
+if(mysqli_num_rows($validar_user) > 0){
+    $_SESSION['usuario']= $usuario;
+    header("Location: deportes.php");
+    exit;
+} else{
+    echo '
+    <script>
+        alert("Usuario no existe");
+        window.location = "index.php"
+    </script>
+    ';
+    exit;
+}
+
+?>
